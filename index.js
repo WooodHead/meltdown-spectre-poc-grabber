bad71c074ff9583d5320c0442f6adcff2c414bc2
willyb321 <wbwilliam7@gmail.com>
2018-01-05 11:56:43 +1100

    make it less nested and terrible

--- index.js
+++ index.js
@@ -5,63 +5,31 @@ const github = new GitHubApi();
 const filterNonUnique = arr =>
 	arr.filter(i => arr.indexOf(i) === arr.lastIndexOf(i));
 const errorAndAttemptOpen = local => Git.Repository.open(local);
+
+const promises = [
+	github.search.repos({q: `meltdown`, sort: 'updated', per_page: 100}),
+	github.search.repos({q: `spectre`, sort: 'updated', per_page: 100})
+];
+
 const meltdownRepos = [];
-github.search
-	.repos({q: `meltdown`, sort: 'updated', per_page: 100})
+Promise.all(promises)
 	.then(repos => {
-		repos.data.items.forEach(elem => {
-			if (!elem.description) {
-				elem.description = elem.full_name;
-			} else {
-				elem.description = `${elem.full_name} - ${elem.description}`;
-			}
-			if (elem.description.toLowerCase().search(/cve/) >= 0) {
-				meltdownRepos.push(elem);
-			}
-			if (elem.description.toLowerCase().search(/exploit/) >= 0) {
-				meltdownRepos.push(elem);
-			}
-			if (elem.description.toLowerCase().search(/attack/) >= 0) {
-				meltdownRepos.push(elem);
-			}
-			if (elem.description.toLowerCase().search(/poc/) >= 0) {
-				meltdownRepos.push(elem);
-			}
-			if (elem.description.toLowerCase().search(/example/) >= 0) {
-				meltdownRepos.push(elem);
-			}
-			if (elem.description.toLowerCase().search(/bug/) >= 0) {
-				meltdownRepos.push(elem);
-			}
+		repos.forEach(data => {
+			data.data.items.forEach(elem => {
+				if (isRelated(elem)) {
+					meltdownRepos.push(elem);
+				}
+			});
 		});
 		const uniqRepos = filterNonUnique(meltdownRepos);
-		const promises = [];
 		uniqRepos.forEach(elem => {
 			Git.Clone(elem.clone_url, `repos/${elem.name}_${elem.owner.login}`)
-				.then(repo => {
+				.then(() => {
 					console.log(`Cloned: ${elem.name}_${elem.owner.login}`);
 				})
 				.catch(err => {
 					if (err.errno === -4) {
-						console.log(`Already cloned, updating: ${elem.name}_${elem.owner.login}`);
-						errorAndAttemptOpen(`repos/${elem.name}_${elem.owner.login}`)
-						.then(repo => {
-							repo.fetchAll().then(() => {
-								repo.mergeBranches('master', 'origin/master')
-									.catch(err => {
-										if (err.errno !== -3) {
-											console.error(err);
-											console.log(`${elem.name}_${elem.owner.login}`);
-										}
-									});
-							  })
-							  .catch(err => {
-								  console.error(err);
-							  });
-						})
-						.catch(err => {
-							console.log(err);
-						});
+						updateRepo(elem);
 					} else {
 						console.log(err);
 					}
@@ -71,66 +39,59 @@ github.search
 	.catch(err => {
 		console.log(err);
 	});
-github.search
-	.repos({q: `spectre`, sort: 'updated', per_page: 100})
-	.then(repos => {
-		repos.data.items.forEach(elem => {
-			if (!elem.description) {
-				elem.description = elem.full_name;
-			} else {
-				elem.description = `${elem.full_name} - ${elem.description}`;
-			}
-			if (elem.description.toLowerCase().search(/cve/) >= 0) {
-				meltdownRepos.push(elem);
-			}
-			if (elem.description.toLowerCase().search(/exploit/) >= 0) {
-				meltdownRepos.push(elem);
-			}
-			if (elem.description.toLowerCase().search(/attack/) >= 0) {
-				meltdownRepos.push(elem);
-			}
-			if (elem.description.toLowerCase().search(/poc/) >= 0) {
-				meltdownRepos.push(elem);
-			}
-			if (elem.description.toLowerCase().search(/example/) >= 0) {
-				meltdownRepos.push(elem);
-			}
-			if (elem.description.toLowerCase().search(/bug/) >= 0) {
-				meltdownRepos.push(elem);
-			}
-		});
-		const uniqRepos = filterNonUnique(meltdownRepos);
-		const promises = [];
-		uniqRepos.forEach(elem => {
-			Git.Clone(elem.clone_url, `repos/${elem.name}_${elem.owner.login}`)
-				.then(repo => {
-					console.log(`Cloned: ${elem.name}_${elem.owner.login}`);
+
+function updateRepo(repoObj) {
+	console.log(
+		`Already cloned, updating: ${repoObj.name}_${repoObj.owner.login}`
+	);
+	errorAndAttemptOpen(`repos/${repoObj.name}_${repoObj.owner.login}`)
+		.then(repo => {
+			repo
+				.fetchAll()
+				.then(() => {
+					repo
+						.mergeBranches(
+							'master',
+							'origin/master'
+						)
+						.catch(err => {
+							if (err.errno !== -3) {
+								console.error(err);
+								console.log(`repos/${repoObj.name}_${repoObj.owner.login} errored.`);
+							}
+						});
 				})
 				.catch(err => {
-					if (err.errno === -4) {
-						console.log(`Already cloned, updating: ${elem.name}_${elem.owner.login}`);
-						errorAndAttemptOpen(`repos/${elem.name}_${elem.owner.login}`)
-						.then(repo => {
-							repo.fetchAll().then(() => {
-								repo.mergeBranches('master', 'origin/master')
-									.catch(err => {
-										console.error(err);
-										console.log(`${elem.name}_${elem.owner.login}`);
-									});
-							  })
-							  .catch(err => {
-								  console.error(err);
-							  });
-						})
-						.catch(err => {
-							console.log(err);
-						});
-					} else {
-						console.log(err);
-					}
+					console.error(err);
 				});
+		})
+		.catch(err => {
+			console.log(err);
 		});
-	})
-	.catch(err => {
-		console.log(err);
-	});
+}
+
+function isRelated(elem) {
+	if (elem.description) {
+		elem.description = `${elem.full_name} - ${elem.description}`;
+	} else {
+		elem.description = elem.full_name;
+	}
+	if (elem.description.toLowerCase().search(/cve/) >= 0) {
+		return true;
+	}
+	if (elem.description.toLowerCase().search(/exploit/) >= 0) {
+		return true;
+	}
+	if (elem.description.toLowerCase().search(/attack/) >= 0) {
+		return true;
+	}
+	if (elem.description.toLowerCase().search(/poc/) >= 0) {
+		return true;
+	}
+	if (elem.description.toLowerCase().search(/example/) >= 0) {
+		return true;
+	}
+	if (elem.description.toLowerCase().search(/bug/) >= 0) {
+		return true;
+	}
+}
